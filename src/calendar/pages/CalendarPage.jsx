524b03c1b6d0e2402ec87c019c9b7723cd9447c3
mzalazar1 <marcoszalazar86@gmail.com>
2024-01-16 00:33:59 -0300

    third commit

--- src/calendar/pages/CalendarPage.jsx
+++ src/calendar/pages/CalendarPage.jsx
@@ -1,9 +1,41 @@
-import { Navbar } from "../components/Navbar";
+import { Calendar } from "react-big-calendar";
+import "react-big-calendar/lib/css/react-big-calendar.css";
+
+import { addHours } from "date-fns";
+import { Navbar } from "../";
+import { localizer, getMessagesES } from "../../helpers";
+
+const events = [
+  {
+    title: "Cumpleaños del jefe",
+    notes: "Hay que comprar el pastel",
+    start: new Date(),
+    end: addHours(new Date(), 2),
+    bgColor: "#fafafa",
+    user: {
+      _id: "123",
+      name: "Fernando",
+    },
+  },
+];
 
 export const CalendarPage = () => {
+  const eventStyleGetter = (event, start, end, isSelected) => {};
+  console.log({ event, start, end, isSelected });
   return (
     <>
       <Navbar />
+
+      <Calendar
+        culture="es"
+        localizer={localizer}
+        events={events}
+        startAccessor="start"
+        endAccessor="end"
+        style={{ height: "calc( 100vh -80px)" }}
+        messages={getMessagesES()}
+        eventPropGetter={eventStyleGetter}
+      />
     </>
   );
 };
