1f4786f3f18d6e1daafd4115c8fa1a2832b2c406
mzalazar1 <marcoszalazar86@gmail.com>
2024-11-26 21:15:03 -0300

    error de data

---
+++ src/hooks/useAuthStore.js
@@ -0,0 +1,34 @@
+import { useDispatch, useSelector } from "react-redux";
+import { calendarApi } from "../api/index";
+import { clearErrorMessage, onChecking, onLogin, onLogout } from "../store";
+
+export const useAuthStore = () => {
+  const { status, user, errorMessage } = useSelector((state) => state.auth);
+  const dispatch = useDispatch();
+
+  const startLogin = async ({ email, password }) => {
+    dispatch(onChecking());
+
+    try {
+      const { data } = await calendarApi.post("/auth", { email, password });
+      localStorage.setItem("token", data.token);
+      localStorage.setItem("token-init-date", new Date().getTime());
+      dispatch(onLogin({ name: data.name, uid: data.uid }));
+    } catch (error) {
+      dispatch(onLogout("Credenciales incorrectas"));
+      setTimeout(() => {
+        dispatch(clearErrorMessage());
+      }, 10);
+    }
+  };
+
+  return {
+    //* Propiedades
+    errorMessage,
+    status,
+    user,
+
+    //*Métodos
+    startLogin,
+  };
+};
